09ffdbaff09e60dc66683bee51b4b1733a05b132
Minh Thuy <minhthuy99cbler@gmail.com>
2025-10-22 22:46:57 +0700

    use albumentations

--- rlp/dataset.py
+++ rlp/dataset.py
@@ -9,53 +9,73 @@ from torch.utils.data import Dataset
 from rlp.utils import load_img, random_add_jpg_compression
 from PIL import Image
 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
 def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in ['jpeg', 'JPEG', 'jpg', 'png', 'JPG', 'PNG', 'gif'])
+    return any(
+        filename.endswith(ext)
+        for ext in ["jpeg", "JPEG", "jpg", "png", "JPG", "PNG", "gif"]
+    )
 
-### rotate and flip
-class Augment_RGB_torch:
-    def __init__(self):
-        pass
-    def transform0(self, torch_tensor):
-        return torch_tensor   
-    def transform1(self, torch_tensor):
-        torch_tensor = torch.rot90(torch_tensor, k=1, dims=[-1,-2])
-        return torch_tensor
-    def transform2(self, torch_tensor):
-        torch_tensor = torch.rot90(torch_tensor, k=2, dims=[-1,-2])
-        return torch_tensor
-    def transform3(self, torch_tensor):
-        torch_tensor = torch.rot90(torch_tensor, k=3, dims=[-1,-2])
-        return torch_tensor
-    def transform4(self, torch_tensor):
-        torch_tensor = torch_tensor.flip(-2)
-        return torch_tensor
-    def transform5(self, torch_tensor):
-        torch_tensor = (torch.rot90(torch_tensor, k=1, dims=[-1,-2])).flip(-2)
-        return torch_tensor
-    def transform6(self, torch_tensor):
-        torch_tensor = (torch.rot90(torch_tensor, k=2, dims=[-1,-2])).flip(-2)
-        return torch_tensor
-    def transform7(self, torch_tensor):
-        torch_tensor = (torch.rot90(torch_tensor, k=3, dims=[-1,-2])).flip(-2)
-        return torch_tensor
-    
-##################################################################################################
+
+def _to_uint8(img: np.ndarray) -> np.ndarray:
+    if img.dtype == np.uint8:
+        return img
+    if img.size == 0:
+        return img
+    m = float(img.max())
+    if m <= 1.0 + 1e-6:
+        img = img * 255.0
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    return img
+
+
+def get_train_transform(ps: int):
+    return A.Compose(
+        [
+            A.RandomResizedCrop(ps, ps, scale=(0.6, 1.0), ratio=(0.8, 1.25)),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(0.15, 0.15, p=0.8),
+            A.Gamma((90, 115), p=0.5),
+            A.HueSaturationValue(
+                hue_shift_limit=4, sat_shift_limit=8, val_shift_limit=8, p=0.3
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+
+def get_eval_transform():
+    return A.Compose([ToTensorV2()])
+
+
 class DatasetTrain(Dataset):
-    def __init__(self, data_dir, input_folder="rainy", gt_folder="gt", img_options=None):
-        super(DatasetTrain, self).__init__()
+    def __init__(
+        self, data_dir, input_folder="rainy", gt_folder="gt", img_options=None
+    ):
+        super().__init__()
 
-        self.augment   = Augment_RGB_torch()
-        self.transforms_aug = [method for method in dir(self.augment) if callable(getattr(self.augment, method)) if not method.startswith('_')] 
-        
         input_filenames = sorted(os.listdir(os.path.join(data_dir, input_folder)))
-        gt_filenames   = sorted(os.listdir(os.path.join(data_dir, gt_folder)))
-        # gt_filenames = [x[:-7]+'.png' for x in input_filenames]
-        
-        self.input_paths = [os.path.join(data_dir, input_folder, x) for x in input_filenames[:500] if is_image_file(x)]
-        self.gt_paths    = [os.path.join(data_dir, gt_folder, x)    for x in gt_filenames[:500]    if is_image_file(x)]
-        
-        self.img_options = img_options
+        gt_filenames = sorted(os.listdir(os.path.join(data_dir, gt_folder)))
+
+        self.input_paths = [
+            os.path.join(data_dir, input_folder, x)
+            for x in input_filenames[:500]
+            if is_image_file(x)
+        ]
+        self.gt_paths = [
+            os.path.join(data_dir, gt_folder, x)
+            for x in gt_filenames[:500]
+            if is_image_file(x)
+        ]
+
+        self.img_options = img_options or {}
+        self.ps = int(self.img_options.get("patch_size", 256))
+
+        self._train_tf = get_train_transform(self.ps)
+        self._eval_tf = get_eval_transform()
 
         self.img_num = len(self.input_paths)
 
@@ -64,53 +84,38 @@ class DatasetTrain(Dataset):
 
     def __getitem__(self, index):
         tar_index = index % self.img_num
-        input = torch.from_numpy(np.float32(load_img(self.input_paths[tar_index])))
-        gt    = torch.from_numpy(np.float32(load_img(self.gt_paths[tar_index])))
 
-        # input = torch.from_numpy(random_add_jpg_compression(input, [35,90]))
-        
-        input = input.permute(2,0,1)
-        gt    = gt.permute(2,0,1)
+        inp_np = np.float32(load_img(self.input_paths[tar_index]))
+        gt_np = np.float32(load_img(self.gt_paths[tar_index]))
+
+        inp_np = _to_uint8(inp_np)
+        gt_np = _to_uint8(gt_np)
 
         input_name = os.path.split(self.input_paths[tar_index])[-1]
-        gt_name    = os.path.split(self.gt_paths[tar_index])[-1]
+        gt_name = os.path.split(self.gt_paths[tar_index])[-1]
 
-        # Random Crop
-        ps = self.img_options['patch_size']
-        H = gt.shape[1]
-        W = gt.shape[2]
-        r = np.random.randint(0, H - ps) if H-ps>0 else 0
-        c = np.random.randint(0, W - ps) if H-ps>0 else 0
-        
-        input = input[:, r:r + ps, c:c + ps]
-        gt    = gt[:, r:r + ps, c:c + ps]
+        data = self._train_tf(image=inp_np, mask=gt_np)
+        input_t = data["image"]
+        gt_t = data["mask"]
 
-        apply_trans = self.transforms_aug[random.getrandbits(3)]
-
-        input = getattr(self.augment, apply_trans)(input)
-        gt    = getattr(self.augment, apply_trans)(gt)
-
-        return input, gt, input_name, gt_name
+        return input_t, gt_t, input_name, gt_name
 
 
-##################################################################################################
 class DatasetTest(Dataset):
     def __init__(self, inp_dir):
-        super(DatasetTest, self).__init__()
-
+        super().__init__()
         inp_files = sorted(os.listdir(inp_dir))
-        self.inp_paths = [os.path.join(inp_dir, x) for x in inp_files if is_image_file(x)]
-
+        self.inp_paths = [
+            os.path.join(inp_dir, x) for x in inp_files if is_image_file(x)
+        ]
         self.inp_num = len(self.inp_paths)
 
     def __len__(self):
         return self.inp_num
 
     def __getitem__(self, index):
-
         inp_path = self.inp_paths[index]
         filename = os.path.splitext(os.path.split(inp_path)[-1])[0]
         inp = Image.open(inp_path)
-
         inp = TF.to_tensor(inp)
         return inp, filename
